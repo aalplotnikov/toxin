6195625e5abd13ca5608e78b3cb291f39d3e9644
Andrey Plotnikov <aalplotnikov@gmail.com>
2021-04-17 22:38:13 +0400

    Added module file-loader and added plugin copy webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 
 const PATHS = {
@@ -43,6 +44,22 @@ module.exports = {
         test: /\.pug$/,
         loader: "pug-loader"
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          outputPath: `${PATHS.assets}/fonts`
+        }
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        exclude: [/fonts/],
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+        }
+      },
     ]
   },
   plugins: [
@@ -56,5 +73,12 @@ module.exports = {
           filename: `./${page.replace(/\.pug$/, '.html')}`
         })
     ),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
+        { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
+        // { from: `${PATHS.src}/static`, to: "" },
+      ]
+    })
   ]
 };
