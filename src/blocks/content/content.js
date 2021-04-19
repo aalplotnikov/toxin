3e396f5efe5f5ed5af37ed255a2db66e72e654b7
Andrey Plotnikov <aalplotnikov@gmail.com>
2021-04-19 11:10:31 +0400

    created calendar

--- src/blocks/content/content.js
+++ src/blocks/content/content.js
@@ -1,8 +1,6 @@
 let element = document.getElementsByClassName('content')[0];
 element.style.height = element.offsetWidth / 1.73493975904 + "px";
-console.log(element)
 window.onresize = function () {
   let element = document.getElementsByClassName('content')[0];
   element.style.height = element.offsetWidth / 1.73493975904 + "px";
-  console.log(element)
 }
